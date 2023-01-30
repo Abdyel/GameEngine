5fef123a517893f85a6ef1e7f33508aa329e2851
Abdyel <Noah@abdyel.com>
2023-01-29 23:27:57 -0600

    created basic implementation on entities, components, and systems

--- GameEngine/src/ECS/ECS.cpp
+++ GameEngine/src/ECS/ECS.cpp
@@ -5,3 +5,26 @@
 int Entity::GetId() const {
 	return id; //encapsulation of member id of entity class.
 }
+
+
+
+void System::AddEntityToSystem(Entity entity) {
+	entities.push_back(entity);
+}
+
+//iterates through entities in system then deletes entities that matches given entity
+void System::RemoveEntitFromSystem(Entity entity) {
+	entities.erase(std::remove_if(entities.begin(), entities.end(), 
+		[&entity](Entity other) {
+			return entity == other;//creates lambda/anonymous function
+		}), entities.end());
+}
+
+std::vector<Entity> System::GetSystemEntities() const {
+	return entities;
+}
+
+const Signature& System::GetComponentSignature() const {
+	return componentSignature;
+}
+
--- GameEngine/src/ECS/ECS.h
+++ GameEngine/src/ECS/ECS.h
@@ -9,29 +9,45 @@
 const unsigned int MAX_COMPONENTS = 32;
 typedef std::bitset<MAX_COMPONENTS> Signature;
 /////////////////////////////////////////////////////////////////////////////
-
+/////////////////////////////////////////////////////////////////////////////
+/// E N T I T Y
+/////////////////////////////////////////////////////////////////////////////
+/// entities are objects that only give an identifier to a game object
+/////////////////////////////////////////////////////////////////////////////
 class Entity {
 	public:
-		Entity(int id) : id(id)//using initializer list to initialized id
-		{
-
-		};
+		Entity(int id) : id(id) {}; //using initializer list to initialized id
+		Entity(const Entity& entity) = default; //overloaded constructor using default build.
 		int GetId() const;//getter method.
+
+		//operator overloading to define the meaning of creating an entities based on another
+		Entity& operator = (const Entity& other) = default;
+		//operator overloading to define the meaning of comparing two entities with ==
+		bool operator == (const Entity& other) const {return id == other.id;}
+		//operator overloaded to define the meaning of comparing two entities with !=
+		bool operator != (const Entity& other) const {return id != other.id;}
+
 	private:
 		int id;
 };
-
-//base structure for component class.
-struct BaseComponent {
-	protected:
+/////////////////////////////////////////////////////////////////////////////
+// C O M P O N E N T
+// //////////////////////////////////////////////////////////////////////////
+//base structure for component class. Interface Component.
+struct IComponent {
+	protected://only usuable to inherited classes or part of class
 		static int nextId;
 };
-
-template <typename T>
-class Component : BaseComponent {
-	
-	int id;
+//used to assign a unique ids to a component type. generic TComponent
+template <typename TComponent>
+class Component : IComponent {
+	// Returns the unique id of the component<T>
+	static int GetId() {
+		static auto id = nextId++;
+		return id;
+	}
 };
+	
 ///////////////////////////////////////////////////////////////////////////
 //  S Y S T E M
 ///////////////////////////////////////////////////////////////////////////
@@ -45,25 +61,34 @@ class System {
 		void AddEntityToSystem(Entity entity);
 		void RemoveEntitFromSystem(Entity entity);
 		std::vector<Entity> GetSystemEntities() const;
-		Signature& GetComponentSignature() const; //returns reference of a Signature
+		const Signature& GetComponentSignature() const; //returns reference of a Signature
 
 		//Define the component type T that entities must have to be considered by the system
 		//generic type, used for any type of component
 		template <typename T> void RequireComponent();
 
-private:
-		Signature componentSignature;//which compents an entity must have for the sistem to consider the enitity
+	private:
+		Signature componentSignature;//which compents an entity must have for the system to consider the enitity
 		std::vector<Entity> entities;//List of all entities that the system is interested in
-};
-
-class Registy {
 
 };
 
-template <typename T>
+//Implements the require component to decide what components matter to a given system
+template <typename TComponent>
 void System::RequireComponent() {
-	const auto componentId = Component<T>::GetId();
-	componentSignature.set(componenetID);
+	const auto componentId = Component<TComponent>::GetId();
+	componentSignature.set(componentId);
 }
+
+//////////////////////////////////////////////////////////////////////////
+/// R E G I S T R Y
+/// //////////////////////////////////////////////////////////////////////
+/// The registry manages the creation and destruction of entities. as well
+/// as adding systems and adding components to entities
+/////////////////////////////////////////////////////////////////////////
+class Registry {
+
+};
+
 
 #endif
