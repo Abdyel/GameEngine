7e2d0353475fe2fed2ddde2889856226519bce45
Abdyel <Noah@abdyel.com>
2023-01-29 18:58:09 -0600

    Added folders and basic header files for basic Entity Component System.

---
+++ GameEngine/src/Systems/MovementSystem.h
@@ -0,0 +1,26 @@
+#ifndef MOVEMENTSYSTEM_H
+#define MOVEMENTSYSTEM_H
+
+//movement system inherits from System class
+class MovementSystem : public System {
+	public:
+		MovementSystem() {
+			//TODO: required Components
+				//Transform Components
+				//Velocity Components
+		}
+		
+		void Update() {
+			//TODO: Logic called frame by frame
+			
+			/*
+				for (auto entity: GetEntities()) {
+					//update position based on its velocity
+					//every frame of the game look
+				}
+			*/
+		}
+};
+
+
+#endif
